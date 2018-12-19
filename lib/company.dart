6dd4237b02234c613f257bb589546b27e490b660
Sam Taylor <samtaylor@192-168-1-9.tpgi.com.au>
2018-12-20 07:48:19 +0800

    Company view widget started

--- lib/company.dart
+++ lib/company.dart
@@ -1,6 +1,8 @@
 class Company {
   final String name;
   final String location;
+  final String image;
+  final String description;
 
-  const Company({this.name, this.location});
+  const Company({this.name, this.location, this.image, this.description});
 }
---
+++ lib/company_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'company.dart';
+
+class CompanyWidget extends StatelessWidget{
+
+  final Company company;
+
+  CompanyWidget(this.company);
+
+  @override
+    Widget build(BuildContext context) {
+      return null;
+    }
+}
