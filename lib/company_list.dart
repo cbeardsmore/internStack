742ceffaaadeb4fe7e46c53c1c616d319631ff08
Sam Taylor <samtaylor@192-168-1-8.tpgi.com.au>
2018-12-21 19:00:45 +0800

    Added Company view widget

--- lib/company_list.dart
+++ lib/company_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'company.dart';
 
 const companies = const <Company>[
-  const Company(name: 'Atlassian', location: 'Sydney'),
+  const Company(name: 'Atlassian', location: 'Sydney', image: atlassian.webp, description: 'Atlassian Corporation Plc is an Australian enterprise software company that develops products'),
   const Company(name: 'Optiver', location: 'Sydney'),
   const Company(name: 'VGW', location: 'Perth'),
   const Company(name: 'Woodside', location: 'Perth'),
--- lib/company_widget.dart
+++ lib/company_widget.dart
@@ -1,14 +1,121 @@
 import 'package:flutter/material.dart';
 import 'company.dart';
 
-class CompanyWidget extends StatelessWidget{
-
+class CompanyWidget extends StatelessWidget {
   final Company company;
 
   CompanyWidget(this.company);
 
   @override
-    Widget build(BuildContext context) {
-      return null;
-    }
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(company.name),
+          ),
+          body: ListView(
+            children: <Widget>[
+              Image.asset(
+                company.image,
+                width: 600.0,
+                height: 240.0,
+                fit: BoxFit.cover,
+              ),
+              TitleSection(company),
+              ButtonSection(),
+              Container(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Text(
+                    company.description,
+                    softWrap: true,
+                  ))
+            ],
+          ),
+        ));
+  }
+}
+
+/* ****************** Title ****************** */
+
+class TitleSection extends StatelessWidget {
+  final Company company;
+
+  TitleSection(this.company);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: titleText(),
+        ),
+        Icon(Icons.star, color: Colors.red),
+        Text('Favourite'),
+      ],
+    );
+  }
+
+  Column titleText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: Text(
+            company.name + 'Office',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Text(
+          company.location,
+          style: TextStyle(color: Colors.grey),
+        ),
+      ],
+    );
+  }
+}
+
+/* ****************** Button ****************** */
+
+class ButtonSection extends StatelessWidget {
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    color = Theme.of(context).primaryColor;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        buildButtonCol(Icons.call, 'CALL'),
+        buildButtonCol(Icons.near_me, 'APPLY'),
+        buildButtonCol(Icons.share, 'SHARE'),
+      ],
+    );
+  }
+
+  Column buildButtonCol(IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ))
+      ],
+    );
+  }
 }
