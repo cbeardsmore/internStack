7506b0f375223c38ca1e287c8d77cbdc70f55eab
Connor Beardsmore <connor.beardsmore@gmail.com>
2018-12-26 07:25:20 +0800

    Company widget appends file path, not database (#11)
    * Company widget appends file path, not database
    
    * Add submit button on listing view and modularise
    
    * Add submission page outline

--- lib/listing.dart
+++ lib/listing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'company_list.dart';
 import 'about.dart';
+import 'submission.dart';
 
 class ListingPage extends StatelessWidget {
   ListingPage({Key key, this.title}) : super(key: key);
@@ -10,23 +11,42 @@ class ListingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        actions: <Widget>[
-            Container(
-              child: IconButton(
-                icon: Icon(Icons.person_outline, color: Colors.white),
-                onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => AboutPage()),
-                );
-              },
-              ),
-            ),
-        ]),
+      appBar: _buildAppBar(context),
       body: CompanyList(),
+      floatingActionButton: _buildFloatingButton(context),
     );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(title: Text(title), actions: <Widget>[
+      Container(
+        child: IconButton(
+          icon: Icon(Icons.person_outline, color: Colors.white),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AboutPage()),
+            );
+          },
+        ),
+      ),
+    ]);
+  }
+
+  Container _buildFloatingButton(BuildContext context) {
+    return new Container(
+        width: 60,
+        height: 60,
+        child: new FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SubmissionPage()),
+            );
+          },
+          tooltip: 'Submit Role',
+          child: new Icon(Icons.add, size: 50),
+        ),
+      );
   }
 }
---
+++ lib/submission.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SubmissionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('internStack - Submit')), body: Text('...'));
+  }
+}
