a46ac1def36ea8992d40f82ac6746e1ab5e29e8a
Connor Beardsmore <connor.beardsmore@vgw.co>
2018-12-26 18:34:33 +0800

    Add logos to images + add listtiles to cards

--- lib/company_list.dart
+++ lib/company_list.dart
@@ -4,7 +4,6 @@ import 'company.dart';
 import 'company_widget.dart';
 
 class CompanyList extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return _buildList(context);
@@ -17,37 +16,44 @@ class CompanyList extends StatelessWidget {
           if (!snapshot.hasData) return const Text('Loading...');
 
           return ListView.builder(
-            itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) {
-                Company company = _documentToCompany(snapshot.data.documents[index]);
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) {
+                Company company =
+                    _documentToCompany(snapshot.data.documents[index]);
                 return _buildListItem(context, company);
-            }
-          );
+              });
         });
   }
 
   Company _documentToCompany(DocumentSnapshot snapshot) {
     return Company(
-      name: snapshot['name'],
-      location: snapshot['location'],
-      image: snapshot['image'],
-      description: snapshot['description']
-    );
+        name: snapshot['name'],
+        location: snapshot['location'],
+        image: snapshot['image'],
+        description: snapshot['description']);
   }
 
-  ListTile _buildListItem(BuildContext context, Company company) {
-    return ListTile(
-      leading: new CircleAvatar(
-        child: new Text(company.name[0]),
+  Card _buildListItem(BuildContext context, Company company) {
+    return Card(
+      color: Colors.grey[300],
+      margin: EdgeInsets.all(9),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: new CircleAvatar(
+            backgroundImage: AssetImage('assets/logo/' + company.name + '.png'),
+            radius: 34,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CompanyWidget(company)),
+            );
+          },
+          title: Text(company.name, style: TextStyle(fontSize: 22)),
+          subtitle: Text(company.location, style: TextStyle(fontSize: 18)),
+        ),
       ),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => CompanyWidget(company)),
-        );
-      },
-      title: Text(company.name),
-      subtitle: Text(company.location),
     );
   }
 }
