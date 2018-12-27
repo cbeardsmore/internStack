6dd73f3d391485c9cca7a507ccd0777f7b00ec86
Connor Beardsmore <connor.beardsmore@gmail.com>
2018-12-27 20:10:51 +0800

    Add base form for submission page

--- lib/submission.dart
+++ lib/submission.dart
@@ -4,6 +4,50 @@ class SubmissionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('internStack - Submit')), body: Text('...'));
+      appBar: AppBar(title: Text('internStack - Submit')),
+      body: SubmitForm(),
+    );
+  }
+}
+
+class SubmitForm extends StatefulWidget {
+  @override
+  SubmitFormState createState() {
+    return SubmitFormState();
+  }
+}
+
+class SubmitFormState extends State<SubmitForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
