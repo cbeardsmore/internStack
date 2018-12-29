6f4090ab0aca1a1f3f99f52bfc219c0c542f1672
Connor Beardsmore <connor.beardsmore@gmail.com>
2018-12-29 17:16:25 +0800

    Add apply link to company (#16)

--- lib/company.dart
+++ lib/company.dart
@@ -3,7 +3,8 @@ class Company {
   final String location;
   final String image;
   final String logo;
+  final String applyLink;
   final String description;
 
-  const Company({this.name, this.location, this.image, this.logo, this.description});
+  const Company({this.name, this.location, this.image, this.logo, this.applyLink, this.description});
 }
