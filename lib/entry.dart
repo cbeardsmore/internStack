1e810b969b60b5ddcb2d6a2a297dcb35a08cec03
Connor Beardsmore <connor.beardsmore@vgw.co>
2019-02-05 18:06:48 +0900

    Refactor entry page and apply correct styling

--- lib/entry.dart
+++ lib/entry.dart
@@ -6,39 +6,60 @@ class EntryPage extends StatelessWidget {
     return new Scaffold(
         body: new Stack(
       children: <Widget>[
-        new Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("assets/entry_background.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        new Container(
-            alignment: AlignmentDirectional(0, -0.7),
-            child: Image.asset(
-              "assets/logo.png",
-              scale: 0.8,
-            )),
-        new Container(
-          alignment: AlignmentDirectional(0, 0.7),
-          child: new SizedBox(
-            height: 50,
-            width: 150,
-            child: new RaisedButton(
-              child: const Text('START',
-                  style: TextStyle(color: Colors.white, fontSize: 20)),
-              color: Color(0xFF302DAD),
-              elevation: 10.0,
-              shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(25.0)),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/listing');
-              },
-            ),
-          ),
-        ),
+        _buildBackground(),
+        _buildLogo(),
+        _buildButton(context),
       ],
     ));
+  }
+
+  Container _buildBackground() {
+    return Container(
+      decoration: new BoxDecoration(
+        image: new DecorationImage(
+          image: new AssetImage("assets/entry_background.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Container _buildLogo() {
+    return Container(
+        alignment: AlignmentDirectional(0, -0.7),
+        child: Image.asset(
+          "assets/logo.png",
+          scale: 0.8,
+        ));
+  }
+
+  Container _buildButton(BuildContext context) {
+    double buttonHeight = MediaQuery.of(context).size.height * 0.065;
+    double buttonWidth = MediaQuery.of(context).size.width * 0.4;
+
+    return Container(
+      alignment: AlignmentDirectional(0, 0.7),
+      child: new SizedBox(
+        height: buttonHeight,
+        width: buttonWidth,
+        child: new RaisedButton(
+          child: const Text('START',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  fontFamily: 'Roboto')),
+          color: Color(0xFF6200EE),
+          splashColor: Color(0xFF25DDC5),
+          elevation: 10.0,
+          highlightElevation: 20.0,
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(20.0)),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/listing');
+          },
+        ),
+      ),
+    );
   }
 }
