d6c5be03346c07ac5fd14948b03f99d880e05b70
Sam Taylor <sammi1616@live.com>
2019-02-06 15:27:06 +0900

    Modified shape of card on listing

--- lib/company_list.dart
+++ lib/company_list.dart
@@ -14,54 +14,64 @@ class CompanyList extends StatelessWidget {
         stream: Firestore.instance.collection('companies').snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData)
-            return new Center(child: CircularProgressIndicator());
+            return Center(child: CircularProgressIndicator());
           return ListView.builder(
               itemCount: snapshot.data.documents.length,
               itemBuilder: (context, index) {
                 Company company =
                     Company.fromDocument(snapshot.data.documents[index]);
-                return _buildListItem(context, company);
+                return _oldbuildListItem(context, company);
               });
         });
   }
 
-  GestureDetector _buildListItem(BuildContext context, Company company) {
-    return new GestureDetector(
+  GestureDetector _oldbuildListItem(BuildContext context, Company company) {
+    return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => CompanyWidget(company)),
         );
       },
-      child: new Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0)),
-          color: Theme.of(context).cardColor,
-          margin: EdgeInsets.all(5),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
-            child: new Row(
-              children: <Widget>[
-                SizedBox(
-                  child: Image.network(company.logo, fit: BoxFit.cover),
-                  height: 50.0,
-                  width: 50.0,
-                ),
-                new Expanded(
-                    child: Column(
+      child: Card(
+            elevation: 6.0,
+            clipBehavior: Clip.antiAlias,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25.0)
+                )),
+            color: Theme.of(context).cardColor,
+            margin: EdgeInsets.all(5),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+              child: Row(
+                
+                children: <Widget>[
+                  _cardImage(company),
+                  Expanded(child: _cardText(context, company))
+                ],
+              ),
+            )),
+    );
+  }
 
-                      children: <Widget>[
-                        Text(company.name,
-                            style: Theme.of(context).textTheme.display1,
-                            textAlign: TextAlign.center),
-                        Text(company.location,
-                            style: Theme.of(context).textTheme.display2,
-                            textAlign: TextAlign.center)
-                      ],
-                    ))
-              ],
-            ),
-          )),
+  SizedBox _cardImage(Company companay) {
+    return SizedBox(
+        child: Image.network(companay.logo, fit: BoxFit.fitHeight),
+        height: 100.0,
+        width: 100.0);
+  }
+
+  Column _cardText(BuildContext context, Company company) {
+    return Column(
+      children: <Widget>[
+        Text(company.name,
+            style: Theme.of(context).textTheme.display1,
+            textAlign: TextAlign.center),
+        Text(company.location,
+            style: Theme.of(context).textTheme.display2,
+            textAlign: TextAlign.center)
+      ],
     );
   }
 }
