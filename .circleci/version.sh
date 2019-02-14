cc0098c09eba4e96211f67a29aadd205a8a9ed57
Connor Beardsmore <connor.beardsmore@vgw.co>
2019-02-14 08:28:39 +0800

    Start of a versioning script to create build number and build name

---
+++ .circleci/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BUILD_NUMBER=${CIRCLE_BUILD_NUM:-'0'}
+BRANCH=${CIRCLE_BRANCH:-'master'}
+
+# If this is a tagged build, the tag is build name, build number is 0
+if ![[ -z "$CIRCLE_TAG" ]]; then
+    echo $CIRCLE_TAG,0
+fi
+
+LASTEST_TAG="$(git rev-list --tags --max-count=1)"
+echo $LASTEST_TAG
+
+# if branch master and not pr -> return blah-beta-buildnumber
+# else return blah-pr-prnumber-buildnumber
+if ![[ -z "$CIRCLE_BRANCH" ]] && ![[ -z "$CIRCLE_PR_NUMBER" ]]; then
+    echo $LASTEST_TAG-beta,$CIRCLE_BUILD_NUM
+else
+    echo $LASTEST_TAG-pr-$CIRCLE_PR_NUMBER,$CIRCLE_BUILD_NUM
+fi
