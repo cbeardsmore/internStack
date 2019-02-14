b7821941303e79b630ed83e00ae2740126114929
Connor Beardsmore <connor.beardsmore@gmail.com>
2019-02-14 19:06:13 +0800

    Echo versions and clean script

--- .circleci/version.sh
+++ .circleci/version.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 REPO_TAG="$(git describe --tags $(git rev-list --tags --max-count=1 2>/dev/null) 2>/dev/null)"
-TAG=${CIRCLE_TAG:-$REPO_TAG}
-BRANCH=${CIRCLE_BRANCH:-'master'}
 
 # tagged version is the TAG
 if [ ! -z "$CIRCLE_TAG" ]; then
     echo $CIRCLE_TAG
 # master build without tag is a BETA
-elif [ "$BRANCH" = "master" ] && [ -z "$CIRCLE_PR_NUMBER" ]; then
-    echo $TAG-beta
+elif [ ! -z ${CIRCLE_BRANCH} ] && [ -z "$CIRCLE_PR_NUMBER" ]; then
+    echo $REPO_TAG-beta
 # must be a PR build
 else
-    echo $TAG-pr
+    echo $REPO_TAG-pr
 fi
