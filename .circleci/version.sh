0965d4d061362d45c0524c81614ba8c55a535456
Connor Beardsmore <connor.beardsmore@vgw.co>
2019-02-14 08:32:48 +0800

    Add script to circleci and echo to test versions are correct

--- .circleci/version.sh
+++ .circleci/version.sh
@@ -5,7 +5,7 @@ BRANCH=${CIRCLE_BRANCH:-'master'}
 
 # If this is a tagged build, the tag is build name, build number is 0
 if ![[ -z "$CIRCLE_TAG" ]]; then
-    echo $CIRCLE_TAG,0
+    echo $CIRCLE_TAG
 fi
 
 LASTEST_TAG="$(git rev-list --tags --max-count=1)"
@@ -14,7 +14,7 @@ echo $LASTEST_TAG
 # if branch master and not pr -> return blah-beta-buildnumber
 # else return blah-pr-prnumber-buildnumber
 if ![[ -z "$CIRCLE_BRANCH" ]] && ![[ -z "$CIRCLE_PR_NUMBER" ]]; then
-    echo $LASTEST_TAG-beta,$CIRCLE_BUILD_NUM
+    echo $LASTEST_TAG-beta
 else
-    echo $LASTEST_TAG-pr-$CIRCLE_PR_NUMBER,$CIRCLE_BUILD_NUM
+    echo $LASTEST_TAG-pr-$CIRCLE_PR_NUMBER
 fi
