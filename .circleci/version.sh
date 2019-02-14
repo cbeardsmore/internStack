6727e2f65d00c2861029f4f20c5aae73f59746ff
Connor Beardsmore <connor.beardsmore@gmail.com>
2019-02-14 18:57:01 +0800

    Keep it simple, bash script for tag versions

--- .circleci/version.sh
+++ .circleci/version.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-COMMIT_TAG="$(git tag -l --points-at HEAD 2>/dev/null)"
-BRANCH_TAG="$(git describe --abbrev=0 --tags 2>/dev/null)"
 REPO_TAG="$(git describe --tags $(git rev-list --tags --max-count=1 2>/dev/null) 2>/dev/null)"
-CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+TAG=${CIRCLE_TAG:-$REPO_TAG}
+BRANCH=${CIRCLE_BRANCH:-'master'}
 
-if [[ -z "$COMMIT_TAG" ]]; then
-    COMMIT_TAG="null"
+# tagged version is the TAG
+if [ ! -z "$CIRCLE_TAG" ]; then
+    echo $CIRCLE_TAG
+# master build without tag is a BETA
+elif [ "$BRANCH" = "master" ] && [ -z "$CIRCLE_PR_NUMBER" ]; then
+    echo $TAG-beta
+# must be a PR build
+else
+    echo $TAG-pr
 fi
-if [[ -z "$BRANCH_TAG" ]]; then
-    BRANCH_TAG="null"
-fi
-if [[ -z "$REPO_TAG" ]]; then
-    REPO_TAG="null"
-fi
-if [[ -z "$CURRENT_BRANCH" ]]; then
-    CURRENT_BRANCH="master"
-fi
-
-pip install semver==2.8.1
-python .circleci/version.py $COMMIT_TAG $BRANCH_TAG $REPO_TAG $CURRENT_BRANCH
