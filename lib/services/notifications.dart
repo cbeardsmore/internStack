7d3571f109a79d00152ededca8f7a89c886fffed
Connor Beardsmore <connor.beardsmore@vgw.co>
2019-03-09 15:16:30 +0800

    Correct style for notification icon + support multiple notifications

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:math';
 
 FlutterLocalNotificationsPlugin notifyPlugin;
+Random random = new Random(DateTime.now().second);
 
 void initialiseNotifications() {
   notifyPlugin = new FlutterLocalNotificationsPlugin();
-  var andriodInitSettings = new AndroidInitializationSettings('@mipmap/ic_launcher');
+  var andriodInitSettings = new AndroidInitializationSettings('app_icon');
   var iosInitSettings = new IOSInitializationSettings(onDidReceiveLocalNotification: null);
   var initSettings = new InitializationSettings(andriodInitSettings, iosInitSettings);
   notifyPlugin.initialize(initSettings, onSelectNotification: null);
@@ -14,8 +16,9 @@ void scheduleNotification(DateTime date, String title, String body) async {
   var andriodNotifyDetails = new AndroidNotificationDetails(
       'Closing Application',
       'internStack',
-      'Notification of Role closing date');
+      'Notification of Role closing date',
+);
   var iosNotifyDetails = new IOSNotificationDetails();
   NotificationDetails platformNotifyDetails = new NotificationDetails(andriodNotifyDetails, iosNotifyDetails);
-  await notifyPlugin.schedule(0, title, body, date, platformNotifyDetails);
+  await notifyPlugin.schedule(random.nextInt(1000), title, body, date, platformNotifyDetails);
 }
