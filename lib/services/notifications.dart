0100d34a1205d3568dd8f0a3d263954c1d3ce496
Connor Beardsmore <connor.beardsmore@vgw.co>
2019-03-09 13:44:45 +0800

    clean up notification naming

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,27 +1,21 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-FlutterLocalNotificationsPlugin notificationsPlugin;
+FlutterLocalNotificationsPlugin notifyPlugin;
 
 void initialiseNotifications() {
-  notificationsPlugin = new FlutterLocalNotificationsPlugin();
-  var initializationSettingsAndroid =
-      new AndroidInitializationSettings('@mipmap/ic_launcher');
-  var initializationSettingsIOS =
-      new IOSInitializationSettings(onDidReceiveLocalNotification: null);
-  var initializationSettings = new InitializationSettings(
-      initializationSettingsAndroid, initializationSettingsIOS);
-  notificationsPlugin.initialize(initializationSettings,
-      onSelectNotification: null);
+  notifyPlugin = new FlutterLocalNotificationsPlugin();
+  var andriodInitSettings = new AndroidInitializationSettings('@mipmap/ic_launcher');
+  var iosInitSettings = new IOSInitializationSettings(onDidReceiveLocalNotification: null);
+  var initSettings = new InitializationSettings(andriodInitSettings, iosInitSettings);
+  notifyPlugin.initialize(initSettings, onSelectNotification: null);
 }
 
 void scheduleNotification(DateTime date, String title, String body) async {
-  var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-      'your other channel id',
-      'your other channel name',
-      'your other channel description');
-  var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
-  NotificationDetails platformChannelSpecifics = new NotificationDetails(
-      androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-  await notificationsPlugin.schedule(0, title, body,
-      date, platformChannelSpecifics);
+  var andriodNotifyDetails = new AndroidNotificationDetails(
+      'Closing Application',
+      'internStack',
+      'Notification of Role closing date');
+  var iosNotifyDetails = new IOSNotificationDetails();
+  NotificationDetails platformNotifyDetails = new NotificationDetails(andriodNotifyDetails, iosNotifyDetails);
+  await notifyPlugin.schedule(0, title, body, date, platformNotifyDetails);
 }
