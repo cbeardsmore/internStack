c001f26c6e533e331bd745285e100f9d15a58276
Connor Beardsmore <connor.beardsmore@vgw.co>
2019-03-10 14:29:41 +0800

    fix missing import

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:math';
 import 'dates.dart';
 
 FlutterLocalNotificationsPlugin notifyPlugin;
